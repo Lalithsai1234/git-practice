d988209f1af78a459a44f1daca6da5bb709ddbe3
Lalith Sai <lalithsaimeesala123@gmail.com>
2026-02-27 14:34:42 +0530

    tester code

--- into.py
+++ into.py
@@ -1,3 +1,4 @@
 print("hello world") #by lalith sai
 print ("welcome to git practice") #by ishwaq
-print("hello world from the updated file") #by shriya 
+print("hello world from the updated file") #by shriya
+print("Merge conflicts")
