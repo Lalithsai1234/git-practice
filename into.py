4e6713089adee99f29273eceb753c81b6555b999
ishwaqsyed03 <ishwaqsyed16@gmail.com>
2026-02-27 14:44:51 +0530

    edited by ishwaq again

--- into.py
+++ into.py
@@ -1,9 +1,8 @@
 print("hello world") #by lalith sai
 print ("welcome to git practice") #by ishwaq
-<<<<<<< HEAD
+
 print("hello world from the updated file") #by shriya 
 print("dljherg")
-=======
+
 print("hello world from the updated file") #by shriya
 print("Merge conflicts")
->>>>>>> d988209f1af78a459a44f1daca6da5bb709ddbe3
